2f5fe833eb932bbf470d455ad368c48a8c2e2aea
Oscar Torres <oscar.torres@hotmail.com>
2021-06-25 18:54:34 -0500

    Base files created

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="main">
+      <p>
+        Hi there
+      </p>
+    </div>
+  );
+}
+
+export default App;
